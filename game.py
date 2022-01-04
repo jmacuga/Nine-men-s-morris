882ad096c1d5f469872f9b8f85ca71b9457e3b63
jmacuga <julia.macuga@gmail.com>
2022-01-05 00:31:54 +0100

    scketch

--- game.py
+++ game.py
@@ -1,2 +1,22 @@
-def pick_board():
-    pass
+from classes import Player, Point, Board
+
+
+
+class Game:
+
+    def check_if_phase_moving(player1, player2, game_mode):
+        if len(player1.occupied()) == game_mode and len(player2.occupied()) == game_mode:
+            return True
+        else:
+            return False
+
+
+    def check_if_win(self):
+        pass
+
+
+    def check_if_phase_flying(player1, player2, game_mode):
+        if len(player2.occupied()) == 3 or len(player1.occupied()) == 3:
+            return True
+        else:
+            return False
---
+++ ui_game.py
@@ -0,0 +1,38 @@
+def pick_board():
+    pass
+
+def pick_player():
+    pass
+
+# phase:
+def make_move(player, phase):
+    """enter coords
+    check cords
+    move - for phase
+    print board
+    check mills -- > remove
+    check phase
+    check win
+    """
+    pass
+def check_mills():
+    pass
+
+def remove_opponent():
+    """
+    input
+    check
+    remove
+    print board"""
+    pass
+
+def check_phase():
+    pass
+
+def check_win():
+    pass
+
+""" reveal winner
+"""
+
+
