eb0756009929a36896c9e763e7fc37533b1b137c
jmacuga <julia.macuga@gmail.com>
2022-01-04 20:55:12 +0100

    phases edited for different boards

--- phases.py
+++ phases.py
@@ -1,22 +1,23 @@
 def placing_pieces():
     """ 1.player1 places a piece
-        2.check if phase2
-        3.check if player gets a point
+        2. if not game_mode 3 : check if phase2
+        3.check if mill
          -> if yes: remove opponent's piece
 
-        4.player2 playces a piece
+        4.player2 places a piece
     """
     pass
 
 
 def moving_pieces():
     """ 1. player1 moves a piece
-        2. check if phase 3
-        3. check if gets a point
+        2. check if gets a point
             -> if yes: remove opponent's piece
-        4. check if move of player2 is possible
-            -> if not: pick a winner
+        3.check win
+        4. if game_mode 9 or 12: check if phase 3
         5. player2 moves a piece
+        6.check win
+        7. if game_mode 9 or 12: check if phase 3
     """
     pass
 
