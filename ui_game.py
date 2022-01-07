c3448bc381f8f1ff260576e4aa3a980f7fcca77f
jmacuga <julia.macuga@gmail.com>
2022-01-07 19:56:33 +0100

    pick_mode corrected

--- ui_game.py
+++ ui_game.py
@@ -2,33 +2,46 @@
 from classes import Point, Player, Board, Mode, ImpossibleMove, CoordsOfNotActivePoint, PointOccupiedError
 from game import Game
 import pytest
+import os
 
 
 def pick_mode():
     """player enters mode"""
-
-    game_mode = input("""Choose Game Mode
+    print("""Choose Game Mode
     1 : Three men's morris
     2 : Six men's morris
     3 : Nine men's morris
-    4 : Twelve men's morris\nMode number:""")
+    4 : Twelve men's morris\n""")
     modes = [1, 2, 3, 4]
-    while (not game_mode.isdigit()) or (not int(game_mode) in modes):
-        print("Wrong mode number, choose from given (1,2,3,4)\n")
+    valid = False
+    while not valid:
         game_mode = input("Mode number:")
+        if  not game_mode.isdigit():
+            print("Wrong mode number, choose from given (1,2,3,4)\n")
+            continue
+        elif not int(game_mode) in modes:
+            print("Wrong mode number, choose from given (1,2,3,4)\n")
+            continue
+        else:
+            valid = True
     return int(game_mode)
 
-
 def main():
+    import os
+    clear = lambda: os.system('clear')
+    clear()
     game_mode = pick_mode()
     game = Game(game_mode)
     player1 = Player(1)
     player2 = Player(2)
     board = game.board()
+    clear()
     print(board.print_board())
     while game.win() == False:
         for player in [player1, player2]:
+            clear()
             game.make_move(player)
+
 
 
 if __name__ == "__main__":
